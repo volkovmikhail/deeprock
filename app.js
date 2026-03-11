e17ab4a450e5187fd4314c2e0641771063d779f4
volkovMikhail <volkov.work1@gmail.com>
2026-03-12 01:26:31 +0300

    change static location

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.locals.basePath = config.basePath;
 const basePath = config.basePath;
 
 app.use(morgan('dev'));
-app.use(basePath + '/static', express.static(path.resolve(__dirname, 'public')));
+app.use(basePath + '/', express.static(path.resolve(__dirname, 'public')));
 app.use(basePath + '/static/shared', express.static(path.resolve(__dirname, 'shared')));
 
 app.use(express.json());
