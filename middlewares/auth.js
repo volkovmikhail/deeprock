267030c85feae0ca002ca197846db6e52c2c34c4
volkovmikhail <volkov.work1@gmail.com>
2026-03-13 00:32:03 +0300

    add tg apps script to index.html

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -15,8 +15,6 @@ function authenticate(req, res, next) {
 
   const initData = req.headers['auth'];
 
-  console.log(req.headers);
-
   if (!initData || typeof initData !== 'string') {
     return res.status(401).json({ error: 'No initData provided in auth header' });
   }
